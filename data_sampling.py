c6cd9c5addbdc352b24bff7aee6338f9d1265743
bankuoniichan <colkeover@gmail.com>
2018-12-09 12:17:32 +0700

    Enarge Samples

--- data_sampling.py
+++ data_sampling.py
@@ -10,17 +10,25 @@ def verify_samples():
 	if not os.path.exists(target_dir):
 		
 		*filelist, = os.listdir(base_dir)
-		*filelist, = sorted(filter(lambda x:x[0]!='.',filelist))
+		*filelist, = sorted(filter(lambda x:x[0]!='.',filelist),key=lambda x:x.lower())
 		filelist = filelist[:100]
+
+		with open('sampling_log.txt','w') as FILE:
+			print(*filelist,sep='\n',file=FILE)
 
 		for d in [target_dir, valid_dir, test_dir]:
 			os.mkdir(d)
 
 		for file in filelist:
-			pd.read_csv(os.path.join(base_dir,file),nrows=1000).to_csv(os.path.join(target_dir,file),index=False)
+			pd.read_csv(os.path.join(base_dir,file),nrows=2000).to_csv(os.path.join(target_dir,file),index=False)
 
 		for file in filelist:
-			pd.read_csv(os.path.join(base_dir,file),nrows=100,skiprows=1100).to_csv(os.path.join(test_dir,file),index=False)
+			pd.read_csv(os.path.join(base_dir,file),nrows=100,skiprows=2100).to_csv(os.path.join(test_dir,file),index=False)
 
 		for file in filelist:
-			pd.read_csv(os.path.join(base_dir,file),nrows=100,skiprows=1200).to_csv(os.path.join(valid_dir,file),index=False)
+			pd.read_csv(os.path.join(base_dir,file),nrows=100,skiprows=2200).to_csv(os.path.join(valid_dir,file),index=False)
+
+
+if __name__=='__main__':
+	print('Verifying ....')
+	verify_samples()
